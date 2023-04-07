0a40ec570a0b81df2d0ea1e876b161ff6e7d44a4
Edison Lee <edisonlee@edisonlee55.com>
2023-04-07 18:27:54 +0800

    feat: update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,15 +45,15 @@ module.exports = {
   protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: '',
+  email: 'mailto:vtuber@nycu.moe',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      name: 'Discord',
+      link: 'https://discord.gg/GPBJPshrBU',
     },
   ],
 
